fbaf316dd4e518344997669d90f75b3148fbe218
Louis_Dev <b05m3r@hotmail.com>
2017-12-19 16:10:46 +0100

    log out button class and fadout

--- web/js/header_Log_Out.js
+++ web/js/header_Log_Out.js
@@ -1,7 +1,15 @@
 $(function() {
-  $("#logOutButton, #closeButton").on("click", function() {
-      $('#logOutMenu').toggleClass('hiddenMenu');
-  });
+  $(".menuHidden").on("click", function() {
+    $("#logOutButton").removeClass('menuHidden')
+    $("#logOutButton").addClass('menuShown')
+    $("#logOutMenu").fadeIn(500);
 
+  });
+  $("#closeButton, .menuShown").on("click", function() {
+    $("#logOutButton").removeClass('menuShown')
+    $("#logOutButton").addClass('menuHidden')
+    $("#logOutMenu").fadeOut(500);
+
+  });
 
   });  // CAUTION leave this at the end of the script CAUTION
