51b777d34acf59f9fbdc403978a5ba25cc2a7cf7
Louis_Dev <b05m3r@hotmail.com>
2017-12-20 17:20:19 +0100

    slide menu changes

--- web/js/header.js
+++ web/js/header.js
@@ -4,21 +4,21 @@ $(function() {
 // for the drop down log out settings
   $("#logOutButton").on("click", function() {
     //$("#logOutMenu").slideDown(500);
-     $("#logOutMenu").show("slide", { direction: "right" }, 1000);
+     $("#logOutMenu").show("slide", { direction: "right" }, 300);
   });
 
   $("#logOutCloseButton").on("click", function() {
-    $("#logOutMenu").hide("slide", { direction: "right" }, 1000);
+    $("#logOutMenu").hide("slide", { direction: "right" }, 300);
   });
 
 
 // for the drop down home menu
   $("#returnMenu").on("click", function() {
-    $("#dropDownOptions").show("slide", { direction: "left" }, 500);
+    $("#dropDownOptions").show("slide", { direction: "left" }, 300);
   });
 
   $("#homeCloseButton").on("click", function() {
-    $("#dropDownOptions").hide("slide", { direction: "left" }, 500);
+    $("#dropDownOptions").hide("slide", { direction: "left" }, 300);
   });
 
 //
